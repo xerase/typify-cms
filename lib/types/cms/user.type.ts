b9b7ef73814998f3ef9581a3310a0dbd99fe0cdd
nimyab <artyomzholobov2004@gmail.com>
2024-10-24 14:18:46 +0300

    fix: user type

--- lib/types/cms/user.type.ts
+++ lib/types/cms/user.type.ts
@@ -1,4 +1,5 @@
 import type { Inn } from './inn.type';
+import type { ProductVariant } from './product-variant.type';
 
 export type AdminRole = 'ADMIN' | 'SUPER_ADMIN';
 
@@ -25,6 +26,33 @@ export type Admin = {
   surname: string | null;
 };
 
+export type Settings = {
+  allowEmail: boolean;
+  allowPhone: boolean;
+
+  onEmail: boolean;
+  onPersonalCabinet: boolean;
+  messageNotification: boolean;
+  orderNotification: boolean;
+  discountNotification: boolean;
+
+  ruPostMainAddressId: number | null;
+  ruPostAddresses: {
+    id: number | string;
+    givenName: string;
+    middleName: string;
+    surname: string;
+    postofficeCode: string | null;
+    houseTo: string;
+    indexTo: number;
+    placeTo: string;
+    regionTo: string;
+    roomTo: string | null;
+    slashTo: string | null;
+    streetTo: string;
+  };
+};
+
 export type User = {
   id: number | string;
   accountType: AccountType;
@@ -38,6 +66,30 @@ export type User = {
   ransom: number;
   role: Role;
   surname: string | null;
+
+  cart: {
+    products: {
+      id: number | string;
+      productVariantArticle: number | string;
+      count: number;
+      productVariant: ProductVariant;
+    }[];
+  } | null;
+  favourite: {
+    products: {
+      id: number | string;
+      productVariantArticle: number | string;
+      productVariant: ProductVariant;
+    }[];
+  } | null;
+  compare: {
+    products: {
+      id: number | string;
+      productVariantArticle: number | string;
+      productVariant: ProductVariant;
+    }[];
+  } | null;
+  settings: Settings;
 
   createdAt: Date;
   updatedAt: Date;
